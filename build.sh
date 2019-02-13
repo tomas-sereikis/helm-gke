af118a0b7df15abf53c890adb4dd8c3f25cf77ac
Tomas Sereikis <tomas.sereikis@gmail.com>
2019-02-13 10:29:38 +0200

    update build image

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 docker build --build-arg VCS_REF=`git rev-parse --short HEAD` \
              --build-arg BUILD_DATE=`date -u +'%Y-%m-%d'` \
-             -t artemkin/helm-gke:`git describe --abbrev=0 --tags` .
+             -t tsereikis/helm-gke:`git describe --abbrev=0 --tags` .
